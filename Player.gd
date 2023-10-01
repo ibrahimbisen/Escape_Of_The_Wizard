03b5efaec2a48323709f4338da25cf4b813bd36a
GirDoggo <101904516+GirDoggo@users.noreply.github.com>
2023-09-30 22:05:46 -0500

    Enemies Type 2
    New Enemy Fires Projectiles at the Player

--- Player.gd
+++ Player.gd
@@ -2,6 +2,10 @@ extends KinematicBody2D
 var speed = 4
 var velocity = Vector2.ZERO
 
+var Projectile = load("res://projectile.tscn")
+signal spawned
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -9,6 +13,16 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if Input.is_action_just_pressed("Shoot"):
+		var projectile = Projectile.instance()
+		var mouse_pos = get_global_mouse_position()
+		
+		projectile.position = global_transform.origin
+		var dir1 = Vector2(mouse_pos.x, mouse_pos.y)
+		var dir2 = Vector2(position)
+		projectile.direction = Vector2(dir1.x-dir2.x, dir1.y-dir2.y).normalized()
+		emit_signal("spawned", projectile)
+
 	if Input.is_action_pressed("move_down"):
 		velocity.y += 1
 		#camera.position.y += 1
@@ -25,3 +39,5 @@ func _process(delta):
 	
 	position += velocity
 	velocity = Vector2.ZERO
+			
+			
