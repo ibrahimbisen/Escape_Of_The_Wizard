169999021be138684430d1b303a0f6a4bdbbdb58
Ibrahim Bisen <33014532+ibrahimbisen@users.noreply.github.com>
2023-10-03 12:41:02 -0500

    movements added

---
+++ Player.gd
@@ -0,0 +1,23 @@
+extends KinematicBody2D
+
+export (int) var speed = 100
+
+func _ready():
+	pass
+
+
+
+func _process(delta):
+	var movement_direction := Vector2.ZERO
+	if Input.is_action_pressed("move_up"):
+		movement_direction.y -= 1
+	if Input.is_action_pressed("move_down"):
+		movement_direction.y += 1
+	if Input.is_action_pressed("move_left"):
+		movement_direction.x -= 1
+	if Input.is_action_pressed("move_right"):
+		movement_direction.x += 1
+	
+	movement_direction = movement_direction.normalized()
+	move_and_slide(movement_direction * speed)
+
