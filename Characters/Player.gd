1d80f2294254b87cc64bf0235b96fa883b847c18
melanieshuynh <90863302+melanieshuynh@users.noreply.github.com>
2023-10-09 13:42:46 -0500

    updated wizard animation
    updates wizard player animation with my art

--- Characters/Player.gd
+++ Characters/Player.gd
@@ -10,6 +10,7 @@ export (int) var speed = 300
 onready var end_of_gun = $EndofGun
 onready var gunpoint = $GunDirection
 onready var magic_shot = $Magic_Shoot
+onready var animPlayer = $AnimationPlayer
 
 var health: int = 100
 
@@ -21,12 +22,19 @@ func _physics_process(delta):
 	var movement_direction := Vector2.ZERO
 	if Input.is_action_pressed("move_up"):
 		movement_direction.y -= 1
+		animPlayer.play("Run")
 	if Input.is_action_pressed("move_down"):
 		movement_direction.y += 1
+		animPlayer.play("Run")
 	if Input.is_action_pressed("move_left"):
 		movement_direction.x -= 1
+		animPlayer.play("Run")
 	if Input.is_action_pressed("move_right"):
 		movement_direction.x += 1
+		animPlayer.play("Run")
+	
+	if movement_direction.x == 0 and movement_direction.y == 0:
+		animPlayer.play("Idle")
 	
 	movement_direction = movement_direction.normalized()
 	move_and_slide(movement_direction * speed)
