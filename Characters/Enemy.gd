43a825feaa983109f2b3fe5b18021360ededc433
Ibrahim Bisen <33014532+ibrahimbisen@users.noreply.github.com>
2023-10-13 11:49:04 -0500

    Revert "Enemy Pathfinding Works Now"

--- Characters/Enemy.gd
+++ Characters/Enemy.gd
@@ -3,37 +3,15 @@ onready var animPlayer = $AnimationPlayer
 
 class_name Enemy
 var health: int = 100
-var speed = 100
+var speed = 40
 var direction = Vector2.ZERO
-var velocity = Vector2.ZERO
 var target
-var can_see
-onready var agent: NavigationAgent2D = $NavigationAgent2D
-var player
+onready var player
 
 var see_thru = [self]
 
 
-func _ready():
-	pass
-	#print(player)
-	#print(self.position)
-	##target = Vector2.ZERO
-	##agent.set_target_location(player.global_position)
-#	nav.set_target_location(self.position)
-
-
 func _physics_process(delta):
-	if agent.is_navigation_finished():
-		return
-	if can_see:
-		direction = global_position.direction_to(agent.get_next_location())
-		#print(player)
-		var desired_velocity = direction * speed
-		var steering = (desired_velocity - velocity) * delta * 4
-		velocity += steering
-		velocity = move_and_slide(velocity)
-		self.rotation = velocity.angle()
 	#the enemy movement needs to be fixed
 	#animPlayer.play("RESET")
 	
@@ -41,35 +19,28 @@ func _physics_process(delta):
 	# import player object for this to work, so it won't run by itself
 	# without main. Also, you must have both objects be of the same type
 	# KinematicBody2D for some reason
-	##var space = get_world_2d().direct_space_state
-	##var enemy_vision = space.intersect_ray(self.global_transform.origin,
-	##								player.global_transform.origin, see_thru)
+	var space = get_world_2d().direct_space_state
+	var enemy_vision = space.intersect_ray(self.global_transform.origin,
+									player.global_transform.origin, see_thru)
 	#print(enemy_vision)
 	
-	##if enemy_vision:
+	if enemy_vision:
 		#print(enemy_vision.collider)
-		##if enemy_vision.collider==player:
-	if player.global_position.distance_to(global_position) < 700:
-	##		target = enemy_vision.collider.global_transform.origin
-	##		direction = (target - position).normalized()
-			#look_at(enemy_vision.collider.global_transform.origin)
-		can_see = true
+		if enemy_vision.collider==player:
+			target = enemy_vision.collider.global_transform.origin
+			direction = (target - position).normalized()
+			look_at(target)
+		else:
+			target = self.position
+			direction = Vector2.ZERO
 	else:
-		can_see = false
-	##		target = self.position
-	##		direction = Vector2.ZERO
-	##else:
-		##target = self.position
-		##direction = Vector2.ZERO
+		target = self.position
+		direction = Vector2.ZERO
 
-##	position += velocity * delta
+	position += direction * speed * delta
 
 
 func handle_hit():
 	health -= 20
 	if health <= 0:
 		queue_free()
-
-
-func _on_NavigationTimer_timeout():
-	agent.set_target_location(player.global_position)
