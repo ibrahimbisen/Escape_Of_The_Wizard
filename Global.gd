42bbbc9fe8d5b6432a3ef667adb1c9336a5a1c38
melanieshuynh <90863302+melanieshuynh@users.noreply.github.com>
2023-10-13 23:31:15 -0500

    new/updated title screen

--- Global.gd
+++ Global.gd
@@ -4,6 +4,8 @@ var max_lives = 5
 var lives = max_lives
 var hud
 
+# Global lose heart function and load HUD to show this change
 func lose_heart():
 	lives -= 1
 	hud.load_hearts()
+	#get_tree().change_scene()
