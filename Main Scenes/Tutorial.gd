60e89ff474bca06d4772539f1a5191c60ddbe31e
Ibrahim Bisen <33014532+ibrahimbisen@users.noreply.github.com>
2023-11-08 15:54:04 -0600

    Fixed launch errors

--- Main Scenes/Tutorial.gd
+++ Main Scenes/Tutorial.gd
@@ -8,58 +8,43 @@ onready var player: Player = $Player
 onready var key1 = $Key
 onready var door1 = $Door
 
-onready var B1 = $BlueAmmo
 onready var B2 = $B1
 onready var B3 = $B2
 onready var B4 = $B3
 onready var B5 = $B4
 onready var B6 = $B5
  
-onready var R1 = $RedAmmo
 onready var R2 = $R1
 onready var R3 = $R2
 onready var R4 = $R3
 onready var R5 = $R4
 
-onready var G1 = $GreenAmmo
 onready var G2 = $G1
 onready var G3 = $G2
 onready var G4 = $G3
 
-onready var enemy = $Enemy
-onready var archer = $Archer_Enemy
-onready var enemy1 = $Archer_Enemy2
-onready var enemy2 = $Archer_Enemy3
-onready var enemy3 = $Archer_Enemy4
-onready var enemy4 = $Archer_Enemy5
-onready var enemy5 = $Archer_Enemy6
-onready var enemy6 = $a_e_7
-onready var enemy7 = $a_e_8
+onready var a_e_1 = $a_e_1
+onready var a_e_2 = $a_e_2
+onready var a_e_3 = $a_e_3
+onready var a_e_4 = $a_e_4
 
 
-onready var rocket2 = $RocketTurret2
-onready var rocket3 = $RocketTurret3
+onready var rocket1 = $RT1
 
-onready var turret2 = $Turret2
-onready var turret3 = $Turret3
-#var EnemyArray = Array()
+onready var turret1 = $T1
 
 
 func _ready():
 	get_tree().call_group("archers", "init_player")
 
-	enemy1.player = player
-
-	enemy3.player = player
-
-	enemy5.player = player
-	enemy6.player = player
+	a_e_1.player = player
+	a_e_2.player = player
+	a_e_3.player = player
+	a_e_4.player = player
 	
-
-	rocket2.player = player
+	rocket1.player = player
+	turret1.player = player
 	
-	turret3.player = player
-
 	key1.door = door1
 	key1.connect("key_picked_up", self, "door_open")
 	
@@ -83,13 +68,13 @@ func _ready():
 	G4.connect("picked_up", self, "ammo_pick_up")
 	
 	
-	rocket2.connect("rocket", self, "handle_enemy_rocket_spawned")
-	turret3.connect("shoot", self, "handle_enemy_bullet_spawned")
+	rocket1.connect("rocket", self, "handle_enemy_rocket_spawned")
+	turret1.connect("shoot", self, "handle_enemy_bullet_spawned")
 	
-	enemy1.connect("shoot", self, "handle_enemy_bullet_spawned")
-	enemy3.connect("shoot", self, "handle_enemy_bullet_spawned")
-	enemy5.connect("shoot", self, "handle_enemy_bullet_spawned")
-	enemy6.connect("shoot", self, "handle_enemy_bullet_spawned")
+	a_e_1.connect("shoot", self, "handle_enemy_bullet_spawned")
+	a_e_2.connect("shoot", self, "handle_enemy_bullet_spawned")
+	a_e_3.connect("shoot", self, "handle_enemy_bullet_spawned")
+	a_e_4.connect("shoot", self, "handle_enemy_bullet_spawned")
 
 
 func handle_enemy_rocket_spawned(bullet: Explosive, a_position: Vector2, direction: Vector2):
